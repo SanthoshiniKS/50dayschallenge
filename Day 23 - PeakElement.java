f226e6692110a44b0513eae8a49e86127e635527
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2024-01-26 20:00:31 +0530

    Create Day 23 - PeakElement.java

--- Day 23 - PeakElement.java
+++ Day 23 - PeakElement.java
@@ -1,7 +1,7 @@
 class Solution
 {
-	public int peakElement(int[] arr,int n)
-  {
+   public int peakElement(int[] arr,int n)
+   {
        int index=0;
        for(int i=0;i<n-1;i++){
            if(arr[i]<arr[i+1])
