4e528dc70e20b896bbd79a7f48988a69c7089909
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2024-02-21 18:46:31 +0530

    Create Day 48 - UglyNumberII.java

---
+++ Day 48 - UglyNumberII.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int nthUglyNumber(int n) {
+        int dp[]=new int[n+1];
+        int d2=1,d3=1,d5=1;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            int m2=dp[d2]*2;
+            int m3=dp[d3]*3;
+            int m5=dp[d5]*5;
+            int min=Math.min(Math.min(m2,m3),m5);
+            dp[i]=min;
+            if(min==m2) d2++;
+            if(min==m3) d3++;
+            if(min==m5) d5++;
+        }
+        return dp[n];
+    }
+}
